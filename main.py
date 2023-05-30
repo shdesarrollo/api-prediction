4c996da343f9321252479858079c1bc838a688e8
Sebastian Hernandez <48390250+shdesarrollo@users.noreply.github.com>
2023-05-30 16:36:39 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,16 +6,12 @@ import uvicorn
 
 app = FastAPI()
 
-origins = [
-    "*"
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
     allow_credentials=True,
     allow_methods=[""],
     allow_headers=[""],
+    allow_origins=['*'],
 )
 
 @app.get("/")
