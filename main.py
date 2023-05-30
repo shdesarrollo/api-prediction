fa6138d415f55121b20530e84598025c20f1dd9f
Sebastian Hernandez <48390250+shdesarrollo@users.noreply.github.com>
2023-05-30 12:05:14 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 from fastapi import FastAPI
 from joblib import load
+from fastapi.middleware.cors import CORSMiddleware
 import sklearn
 import uvicorn
 
 app = FastAPI()
+
+origins = [
+    "http://localhost/",
+    "http://localhost:3000/"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=[""],
+    allow_headers=[""],
+)
 
 @app.get("/")
 async def root():
